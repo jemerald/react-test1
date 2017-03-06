c3312fd039bb9e7254d9546eb932967eacbfc44d
Jackie Weng <jackie@jemeraldsoft.co.nz>
2017-03-06 22:14:39 +1300

    Working

--- src/components/AthletePreview.js
+++ src/components/AthletePreview.js
@@ -9,7 +9,7 @@ export default class AthletePreview extends React.Component {
       <Link to={`/athlete/${this.props.id}`}>
         <div className="athlete-preview">
           <img src={`img/${this.props.image}`}/>
-          <h2 className="name">{this.props.name}</h2>
+          <h2 className="name">{this.props.name} test</h2>
           <span className="medals-count"><img src="/img/medal.png"/> {this.props.medals.length}</span>
         </div>
       </Link>
